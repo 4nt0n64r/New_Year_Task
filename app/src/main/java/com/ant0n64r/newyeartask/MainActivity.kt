2722118735c9d4a2b1e7240eadaa7892719bf5ce
Anton Sharapov <cheel4069@gmail.com>
2019-01-11 16:46:20 +0200

    add paradigms

--- app/src/main/java/com/ant0n64r/newyeartask/MainActivity.kt
+++ app/src/main/java/com/ant0n64r/newyeartask/MainActivity.kt
@@ -36,16 +36,16 @@ class MainActivity : AppCompatActivity() {
 
         val languaches = ArrayList<ProgrammingLanguage>()
 
-        languaches.add(ProgrammingLanguage("Java", "Джеймс\nГослинг", 1995, "????", java, 1, "https://ru.wikipedia.org/wiki/Java"))
-        languaches.add(ProgrammingLanguage("C", "Деннис Ритчи,\nКен Томпсон", 1972, "????", c, 2, "https://en.wikipedia.org/wiki/C_(programming_language)"))
-        languaches.add(ProgrammingLanguage("Python", "Гвидо\nВан Россум", 1990, "????", python, 3, "https://ru.wikipedia.org/wiki/Python"))
-        languaches.add(ProgrammingLanguage("С++", "Бьёрн\nСтрауструп", 1983, "????", cplusplus, 4, "https://ru.wikipedia.org/wiki/C%2B%2B"))
-        languaches.add(ProgrammingLanguage("Visual Basic", "Microsoft", 1991, "????", visualbasic, 5, "https://ru.wikipedia.org/wiki/Visual_Basic"))
-        languaches.add(ProgrammingLanguage("Java Script", "Брендан Эйх", 1995, "????", javascript, 6, "https://ru.wikipedia.org/wiki/JavaScript"))
-        languaches.add(ProgrammingLanguage("C#", "Андерс\nХейлсберг", 2000, "????", csharp, 7, "https://ru.wikipedia.org/wiki/C_Sharp"))
-        languaches.add(ProgrammingLanguage("PHP", "Расмус\nЛердорф", 1995, "????", php, 8, "https://ru.wikipedia.org/wiki/PHP"))
-        languaches.add(ProgrammingLanguage("SQL", "Д. Чемберлин,\nР. Бойс", 1974, "????", sql, 9, "https://ru.wikipedia.org/wiki/SQL"))
-        languaches.add(ProgrammingLanguage("Objective C", "Бред\nКокс", 1983, "????", objectivec, 10, "https://ru.wikipedia.org/wiki/Objective-C"))
+        languaches.add(ProgrammingLanguage("Java", "Джеймс\nГослинг", 1995, "Imperative, Structured, Functional, Object-oriented", java, 1, "https://ru.wikipedia.org/wiki/Java"))
+        languaches.add(ProgrammingLanguage("C", "Деннис Ритчи,\nКен Томпсон", 1972, "Imperative, Structured, Procedural", c, 2, "https://en.wikipedia.org/wiki/C_(programming_language)"))
+        languaches.add(ProgrammingLanguage("Python", "Гвидо\nВан Россум", 1990, "Imperative, Structured, Procedural, Functional, Object-oriented", python, 3, "https://ru.wikipedia.org/wiki/Python"))
+        languaches.add(ProgrammingLanguage("С++", "Бьёрн\nСтрауструп", 1983, "Imperative, Structured, Procedural, Functional, Object-oriented", cplusplus, 4, "https://ru.wikipedia.org/wiki/C%2B%2B"))
+        languaches.add(ProgrammingLanguage("Visual Basic", "Microsoft", 1991, "Event-driven", visualbasic, 5, "https://ru.wikipedia.org/wiki/Visual_Basic"))
+        languaches.add(ProgrammingLanguage("Java Script", "Брендан Эйх", 1995, "Functional, Event-driven, Object-oriented", javascript, 6, "https://ru.wikipedia.org/wiki/JavaScript"))
+        languaches.add(ProgrammingLanguage("C#", "Андерс\nХейлсберг", 2000, "Object-oriented", csharp, 7, "https://ru.wikipedia.org/wiki/C_Sharp"))
+        languaches.add(ProgrammingLanguage("PHP", "Расмус\nЛердорф", 1995, "Imperative, Structured, Procedural, Object-oriented", php, 8, "https://ru.wikipedia.org/wiki/PHP"))
+        languaches.add(ProgrammingLanguage("SQL", "Д. Чемберлин,\nР. Бойс", 1974, "Declarative", sql, 9, "https://ru.wikipedia.org/wiki/SQL"))
+        languaches.add(ProgrammingLanguage("Objective C", "Бред\nКокс", 1983, "Object-oriented", objectivec, 10, "https://ru.wikipedia.org/wiki/Objective-C"))
 
         val adapter = CustomAdapter(languaches)
 
