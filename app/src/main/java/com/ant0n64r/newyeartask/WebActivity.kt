707339bb6001bdf4ab7cf359883b74717bb49dfb
Anton Sharapov <cheel4069@gmail.com>
2019-01-13 19:45:55 +0200

    update webView to open links in app

--- app/src/main/java/com/ant0n64r/newyeartask/WebActivity.kt
+++ app/src/main/java/com/ant0n64r/newyeartask/WebActivity.kt
@@ -2,7 +2,9 @@ package com.ant0n64r.newyeartask
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_web.*
+import kotlinx.android.synthetic.main.activity_web.view.*
 
 class WebActivity : AppCompatActivity() {
 
@@ -10,7 +12,7 @@ class WebActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
 
-
+        webView.setWebViewClient(WebViewClient())
         webView.loadUrl(intent.getStringExtra("LINK"))
     }
 }
